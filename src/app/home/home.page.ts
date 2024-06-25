f7bb707b87ce22fe5a7f67ce4804017085736ccf
Yuseydi <pamelacamisan26@gmail.com>
2024-06-25 00:09:19 -0500

    Adaptando input y label a ion-input

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
 
   constructor() {}
 
+  ngAfterViewInit() {
+    const togglePassword = document.getElementById('togglePassword');
+    const passwordInput = document.getElementById('password');
+
+    if (togglePassword && passwordInput) {
+      togglePassword.addEventListener('click', function () {
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+        this.classList.toggle('fa-eye');
+        this.classList.toggle('fa-eye-slash');
+      });
+    }
+  }
 }
